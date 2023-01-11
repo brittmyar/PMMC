7d31791eec9b00b8b0170fd9943a9172d32cdcab
Brittany Yarbrough <brittanymyar@gmail.com>
2023-01-11 15:58:52 -0500

    Create a base.html inside of the template folder

--- main_app/views.py
+++ main_app/views.py
@@ -4,8 +4,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Define the home view
+
+
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
 
 def about(request):
-  return HttpResponse('<h1>About the CatCollector</h1>')
+    return render(request, 'about.html')
